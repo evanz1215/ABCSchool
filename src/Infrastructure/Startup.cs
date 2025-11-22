662caa6c32ce6c7ceeb783676cd6e2cdb20c2db7
Evan <evanz1215@gmail.com>
2025-11-23 01:23:48 +0800

    新增多租戶支持及刪除未使用的類別
    刪除了 `Class1.cs` 中的 `Class1` 類別，清理未使用的代碼。
    在 `appsettings.json` 中新增了資料庫連接字串配置 `DefaultConnection`。
    新增 `Startup` 類別，提供 `AddMultitenancyServices` 擴展方法，用於配置多租戶服務，包括資料庫連接、多租戶策略及存儲機制。
    新增 `TenantDbContext` 類別，配置多租戶資料表 `Tenants`，並設置表的架構為 `Multitenancy`。

---
+++ src/Infrastructure/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Finbuckle.MultiTenant;
+using Infrastructure.Tenancy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class Startup
+{
+    public static IServiceCollection AddMultitenancyServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<TenantDbContext>(options =>
+        {
+            options
+            .UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        })
+            .AddMultiTenant<ABCSchoolTenantInfo>()
+            .WithHeaderStrategy("tenant")
+            .WithClaimStrategy("tenant")
+            .WithEFCoreStore<TenantDbContext, ABCSchoolTenantInfo>();
+
+        return services;
+    }
+}
