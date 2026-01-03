fe82cc28db41c10d9545915e16791f3c94c16af9
Evan <evanz1215@gmail.com>
2026-01-03 22:20:29 +0800

    新增學校查詢功能 (依名稱及全部查詢)
    新增 GetSchoolByNameQuery 及 GetSchoolsQuery 兩個查詢功能，支援依學校名稱查詢單一學校及查詢所有學校。採用 MediatR 實作 CQRS 架構，透過 ISchoolService 取得資料，並以 Mapster 進行型別轉換，最後用 ResponseWrapper 封裝回應結果。

---
+++ src/core/Applocation/Features/Schools/Queries/GetSchoolByNameQuery.cs
@@ -0,0 +1,33 @@
+﻿using Applocation.Wrappers;
+using Mapster;
+using MediatR;
+
+namespace Applocation.Features.Schools.Queries;
+
+public class GetSchoolByNameQuery : IRequest<IResponseWrapper>
+{
+    public string Name { get; set; }
+}
+
+public class GetSchoolByNameQueryHandler : IRequestHandler<GetSchoolByNameQuery, IResponseWrapper>
+{
+    private readonly ISchoolService _schoolService;
+
+    public GetSchoolByNameQueryHandler(ISchoolService schoolService)
+    {
+        _schoolService = schoolService;
+    }
+
+    public async Task<IResponseWrapper> Handle(GetSchoolByNameQuery request, CancellationToken cancellationToken)
+    {
+        var schoolInDb = await _schoolService.GetByNameAsync(request.Name);
+
+        if (schoolInDb is not null)
+        {
+            var result = schoolInDb.Adapt<SchoolResponse>();
+            return await ResponseWrapper<SchoolResponse>.SuccessAsync(result);
+        }
+
+        return await ResponseWrapper<SchoolResponse>.FailAsync($"School with name {request.Name} not found.");
+    }
+}
---
+++ src/core/Applocation/Features/Schools/Queries/GetSchoolsQuery.cs
@@ -0,0 +1,27 @@
+﻿using Applocation.Wrappers;
+using Mapster;
+using MediatR;
+
+namespace Applocation.Features.Schools.Queries;
+
+public class GetSchoolsQuery : IRequest<IResponseWrapper>
+{
+}
+
+public class GetSchoolsQueryHandler : IRequestHandler<GetSchoolsQuery, IResponseWrapper>
+{
+    private readonly ISchoolService _schoolService;
+
+    public GetSchoolsQueryHandler(ISchoolService schoolService)
+    {
+        _schoolService = schoolService;
+    }
+
+    public async Task<IResponseWrapper> Handle(GetSchoolsQuery request, CancellationToken cancellationToken)
+    {
+        var schoolsInDb = await _schoolService.GetAllAsync();
+
+        var schoolMappingResult = schoolsInDb?.Adapt<List<SchoolResponse>>() ?? new List<SchoolResponse>();
+        return await ResponseWrapper<List<SchoolResponse>>.SuccessAsync(schoolMappingResult);
+    }
+}
